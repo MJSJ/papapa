58cd48cabad55bc601c250a987e6972442b61778
zhang.yufeng <420866750@qq.com>
2017-04-26 20:31:14 +0800

    dev

--- index.js
+++ index.js
@@ -17,13 +17,16 @@
       preload.addEventListener("fileload", handleFileComplete);
       preload.loadManifest([
         { "id": "bg0", "src": "../imgs/bg0.jpg" },
+        { "id": "bg1", "src": "../imgs/bg1.jpg" },
         { "id": "girl", "src": "../imgs/girl.png" },
+        { "id": "eyes", "src": "../imgs/eyes.png" },
         { "id": "candle1", "src": "../imgs/candles.png" },
         { "id": "finger", "src": "../imgs/finger.png" },
         { "id": "bianzi", "src": "../imgs/bianzi.png" },
         { "id": "bianzi2", "src": "../imgs/bianzi2.png" },
         { "id": "pabtn", "src": "../imgs/beginbtn.png" },
-        { "id": "pa", "src": "../imgs/pa.png" }
+        { "id": "pa", "src": "../imgs/pa.png" },
+        { "id": "star", "src": "../imgs/star.png" }
       ]);
       function handleFileComplete() {
         if(preload._numItems == preload._numItemsLoaded){
@@ -45,7 +48,13 @@
     this.stage = new createjs.Stage(id),
     this.playing = false,
     this.loader = loader,
-    this.isTemp = false,
+    this.alltemp = 0, // 节奏总数
+    this.correct = 0, // 踩对的节奏数
+    this.dire = [
+      {x: 259, y: 547 },
+      {x: 364, y: 637 },
+      {x: 528, y: 738 },
+    ]
     this.time = 1000;
   }
   
@@ -57,7 +66,7 @@
     this.insTimer = undefined;
     this.insAdo = undefined;    
     
-    this.drawGame();
+    this.drawMain();
   }
 
   // 首页
@@ -75,7 +84,6 @@
     function handleComplete() {
         _this.drawMain();
     }
-    // this.stage.addChild(blackbg);
     this.stage.addChild(bg0);
   }
 
@@ -83,15 +91,13 @@
   Stage.prototype.drawMain = function(){
     this.stage.removeAllChildren();
     // 背景
-    var blackbg = new createjs.Shape();
-    blackbg.graphics.beginFill("#434343").drawRect(0, 0, 750, 1336);
+    var blackbg = this.getBitMap({
+      x: 0, y: 0, width: 750, height: 1336, id: 'bg1'
+    });
     // 主角
-    var girl = new createjs.Bitmap(this.loader.getResult("girl"));
-    girl.width = 662;
-    girl.height = 792;
-    girl.x = 50;
-    girl.y = 250;
-    // createjs.Tween.get(girl, {loop:true}).to({alpha:0.5, x: 55, y: 260},1000).to({alpha:1, x: 50, y: 250},1000).call(handleComplete);
+    var girl = this.getBitMap({
+      width: 736, height: 832, x: 14, y: 250, id: 'girl'
+    });
     function handleComplete() {}
     // 蜡烛
     var cdSheet = new createjs.SpriteSheet({
@@ -107,29 +113,39 @@
     var cdani = new createjs.Sprite(cdSheet, "burn");
     cdani.x = cdani.y = 30;
     // 手指和鞭子
-    var finger = new createjs.Bitmap(this.loader.getResult("finger")),
-        bianzi = new createjs.Bitmap(this.loader.getResult("bianzi")),
-        pabtn  = new createjs.Bitmap(this.loader.getResult("pabtn"));
-    bianzi.x = 300;
-    bianzi.y = 400;
-    bianzi.width = 152;
-    bianzi.height = 280;
-    finger.width = 172;
-    finger.height= 153;
-    finger.x = 600;
-    finger.y = 500;
-    bianzi.visible = false;
-    pabtn.x = 160;
-    pabtn.y = 680;
-    pabtn.width = 456;
-    pabtn.height = 273;
-    pabtn.alpha = 0;
-    pabtn.visible = false;
+    var finger = this.getBitMap({
+      x: 600, y: 500, width: 172, height: 153, id: 'finger'
+    }),
+    bianzi = this.getBitMap({
+      x: 300, y: 400, width: 152, height: 280, visible: false, id: 'bianzi'
+    }),
+    pabtn = this.getBitMap({
+      x: 160, y: 680, width: 456, height: 273, alpha: 0, visible: false, id: 'pabtn'
+    });
+    // 表情
+    var faces = new createjs.SpriteSheet({
+        images: ['../imgs/eyes.png'],
+        frames: {width:89.333, height:99, count: 3},
+        animations: {
+            hurt:0,
+            amaze:1,
+            fear:2
+        }
+    });
+    var fear = new createjs.Sprite(faces, "fear"),
+        amaze = new createjs.Sprite(faces, "amaze"),
+        hurt = new createjs.Sprite(faces, "hurt");
+    fear.x = amaze.x = hurt.x = 172;
+    fear.y = amaze.y = hurt.y = 325;
+    amaze.visible = hurt.visible = false;
+    // 手指和鞭子切换
     createjs.Tween.get(pabtn, {loop:true}).to({alpha: 1},500).to({alpha: 0.5},500).call(handleComplete);
     function handleCompleteF(){
       finger.visible = false;
       bianzi.visible = true;
       pabtn.visible = true;
+      amaze.visible = true;
+      fear.visible = hurt.visible = false;
       createjs.Tween.get(bianzi).to({x: 200, y: 400},1000).call(handleCompleteF2);
     }
     function handleCompleteF2(){
@@ -139,6 +155,8 @@
       pabtn.visible = false;
       finger.visible = true;
       bianzi.visible = false;
+      fear.visible = true;
+      amaze.visible = hurt.visible = false;
       createjs.Tween.get(finger).to({x: 600, y: 500},600).call(handleCompleteF4);
     }
     function handleCompleteF4(){
@@ -146,12 +164,15 @@
     }
     handleCompleteF4();
     
-    this.stage.addChildAt(blackbg, 0);
-    this.stage.addChildAt(girl, 1);
-    this.stage.addChildAt(cdani, 2);
-    this.stage.addChildAt(bianzi, 3);
-    this.stage.addChildAt(finger, 4);
-    this.stage.addChildAt(pabtn, 5);
+    this.stage.addChild(blackbg);
+    this.stage.addChild(girl);
+    this.stage.addChild(fear);
+    this.stage.addChild(amaze);
+    this.stage.addChild(hurt);
+    this.stage.addChild(cdani);
+    this.stage.addChild(bianzi);
+    this.stage.addChild(finger);
+    this.stage.addChild(pabtn);
 
     // 点击开始
     _this = this;
@@ -164,9 +185,12 @@
   Stage.prototype.drawGame = function(){
     _this = this;
     _this.stage.removeAllChildren();
+    _this.correct = 0;
+    _this.alltemp = 0;
     // 背景
-    var blackbg = new createjs.Shape();
-    blackbg.graphics.beginFill("#434343").drawRect(0, 0, 750, 1336);
+    var blackbg = this.getBitMap({
+      x: 0, y: 0, width: 750, height: 1336, id: 'bg1'
+    });
     // 蜡烛
     var cdSheet = new createjs.SpriteSheet({
         images: ['../imgs/candles.png'],
@@ -181,91 +205,93 @@
     var cdani = new createjs.Sprite(cdSheet, "burn");
     cdani.x = cdani.y = 30;
     // 主角
-    var girl = new createjs.Bitmap(this.loader.getResult("girl"));
-    girl.width = 662;
-    girl.height = 792;
-    girl.x = 50;
-    girl.y = 250;
+    var girl = this.getBitMap({
+      width: 736, height: 832, x: 14, y: 250, id: 'girl'
+    });
     // 鞭子
-    var bianzi = new createjs.Bitmap(this.loader.getResult("bianzi"));
-    bianzi.x = 540;
-    bianzi.y = 580;
-    bianzi.width = 152;
-    bianzi.height = 280;
-    bianzi.regX = 152;
-    bianzi.regY = 280;
-    var bianzi2 = new createjs.Bitmap(this.loader.getResult("bianzi2"));
-    bianzi2.x = 540;
-    bianzi2.y = 630;
-    bianzi2.width = 344;
-    bianzi2.height = 206;
-    bianzi2.regX = 344;
-    bianzi2.regY = 206;
-    bianzi2.rotation = 0;
-    bianzi2.visible = false;
-    // 啪
-    var pa = new createjs.Bitmap(this.loader.getResult("pa"));
-    pa.x = 350;
-    pa.y = 650;
-    pa.width = 187;
-    pa.height = 181;
-    pa.regX = 83.5;
-    pa.regY = 90.5;
-    pa.visible = false;
+    var bianzi = this.getBitMap({
+      x: 540,y: 580,width: 152,height: 280,regX: 152,regY: 280, id: 'bianzi'
+    }),
+    bianzi2 = this.getBitMap({
+      x: 540,y: 630,width: 344,height: 206,regX: 344,regY: 206,visible: false, id: 'bianzi2'
+    });
+    pa = this.getBitMap({
+      x: 350,y: 650,width: 187,regX: 83.5,regY: 90.5,visible: false, id: 'pa'
+    });
 
     _this.getAdo().getInstance();
-    function handleCompleteP(){
-      pa.visible = false;
-      pa.scaleX = pa.scaleY = 1;
-      pa.alpha = 1;
-    }
-    function handleCompleteB2(){
-      bianzi.visible = true;
-      bianzi2.visible = false;
-      pa.visible = true;
-      createjs.Tween.get(pa).to({alpha: 0.5, scaleX: 2, scaleY: 2},500).call(handleCompleteP);
-    }
-    function handleCompleteB(){
-      bianzi.rotation = 0;
-      bianzi.visible = false;
-      bianzi2.visible = true;
-      createjs.Tween.get(bianzi2).to({rotation: -10},100).call(handleCompleteB2);
-    }
-    blackbg.addEventListener('click', function(e){
-      createjs.Tween.get(bianzi).to({rotation: -80},200).call(handleCompleteB);
-      createjs.Tween.get(girl).to({x: 30},100).to({x: 60},100).to({x: 50},100).call(handleComplete);
-      console.log(_this.isTemp);
+    // function handleComplete(){}
+    // function handleCompleteP(){
+    //   pa.visible = false;
+    //   pa.scaleX = pa.scaleY = 1;
+    //   pa.alpha = 1;
+    // }
+    // function handleCompleteB2(){
+    //   bianzi.visible = true;
+    //   bianzi2.visible = false;
+    //   pa.visible = true;
+    //   createjs.Tween.get(pa).to({alpha: 0.5, scaleX: 2, scaleY: 2},500).call(handleCompleteP);
+    // }
+    // function handleCompleteB(){
+    //   bianzi.rotation = 0;
+    //   bianzi.visible = false;
+    //   bianzi2.visible = true;
+    //   createjs.Tween.get(bianzi2).to({rotation: -10},100).call(handleCompleteB2);
+    // }
+    // blackbg.addEventListener('click', function(e){
+    //   createjs.Tween.get(bianzi).to({rotation: -80},200).call(handleCompleteB);
+    //   createjs.Tween.get(girl).to({x: 0},100).to({x: 30},100).to({x: 14},100).call(handleComplete);
+    // });
+    var _this = this;
+    this.star = this.getBitMap({
+      x: 0, y: 0, width: 146, height: 134, visible: false, id: 'star', regX: 73, regY: 67
+    });
+    blackbg.addEventListener('mousedown', function(e){
+      alert("!")
+      _this.correct++;
     });
 
-    this.stage.addChildAt(blackbg, 0);
-    this.stage.addChildAt(girl, 1);
-    this.stage.addChildAt(cdani, 2);
-    this.stage.addChildAt(bianzi, 3);
-    this.stage.addChildAt(bianzi2, 3);
-    this.stage.addChildAt(pa, 3);
+    this.stage.addChild(blackbg);
+    this.stage.addChild(girl);
+    this.stage.addChild(cdani);
+    this.stage.addChild(bianzi);
+    this.stage.addChild(bianzi2);
+    this.stage.addChild(pa);
+    this.stage.addChild(this.star);
   }
 
-  // 节奏小球
-  Stage.prototype.drawTemp = function(){
-    this.circle = new createjs.Shape();
-    this.circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    this.circle.x = 100;
-    this.circle.y = 100;
-
-    this.circle.addEventListener('click', function(e){
-      _this.getAdo().getInstance()
-    });
-    this.stage.addChild(this.circle);
+  // 获取bitmap
+  Stage.prototype.getBitMap = function(conf){
+    var bitmap = new createjs.Bitmap(this.loader.getResult(conf.id));
+    if (conf.visible !== false) conf.visible = true;
+    bitmap.x = conf.x || 0;
+    bitmap.y = conf.y || 0;
+    bitmap.width = conf.width || 0;
+    bitmap.height = conf.height || 0;
+    bitmap.regX = conf.regX || 0;
+    bitmap.regY = conf.regY || 0;
+    bitmap.visible = conf.visible;
+    bitmap.alpha = conf.alpha || 1;
+    return bitmap;
   }
 
   // 寻找节奏
   Stage.prototype.checkTemp = function(){
     var _this = this;
-    // 2200 3100
-    if(this.time % 900 > 350 && this.time % 900 < 450){
-      _this.isTemp = true;
+    function handleCompleteP(){
+      _this.star.visible = false;
+      _this.star.scaleX = _this.star.scaleY = 1;
+      _this.star.alpha = 1;
+    }
+    if(this.time % 900 == 100){
+      _this.alltemp++;
+      _this.star.visible = true;
+      var pos = _this.dire[parseInt(Math.random()*3)]
+      _this.star.x = pos.x;
+      _this.star.y = pos.y;
+      createjs.Tween.get(this.star).to({alpha: 0.5, scaleX: 1.5, scaleY: 1.5},200).call(handleCompleteP);
     }else{
-      _this.isTemp = false;
+      
     }
   }
 
@@ -275,8 +301,9 @@
     return (function(_this){
       function init(){
         var timer = setInterval(function(){
-          if(_this.time >　240000){
+          if(_this.time >　24000){
             clearInterval(timer)
+            alert(_this.correct +' '+ _this.alltemp)
           }else{
             _this.time += 20
             _this.checkTemp()
